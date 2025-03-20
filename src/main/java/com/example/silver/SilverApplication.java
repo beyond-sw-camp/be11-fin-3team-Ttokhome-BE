287af714e2aed88cab2ed182fde78627e1cc7634
issac-cosmos <rud____@naver.com>
2025-03-20 16:21:45 +0900

    fir

---
+++ src/main/java/com/example/silver/SilverApplication.java
@@ -0,0 +1,13 @@
+package com.example.silver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverApplication.class, args);
+	}
+
+}
